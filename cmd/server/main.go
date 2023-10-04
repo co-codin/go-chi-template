8fe7ecf54df219e2b07dffcfaf32fca6664ecbfb
elijah <cuiyeqing960904@gmail.com>
2023-10-04 07:33:45 -0400

    db connect

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	Port string
+}
+
+type Application struct {
+	Config Config
+}
+
+var port = os.Getenv("PORT")
+
+func (app *Application) Serve() error {
+	fmt.Println("API listening on port", port)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", port),
+	}
+
+	return srv.ListenAndServe()
+}
+
 func main() {
-	
+	var cfg Config
+
+	cfg.Port = port
+
+	dsn := os.Getenv("DSN")
 }
--- db/db.go
+++ db/db.go
@@ -1,1 +1,51 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+type DB struct {
+	DB *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDbConn = 10
+const maxIdleDbConn = 5
+const maxDbLifeTime = 5 * time.Minute
+
+
+func ConnectPostgres(dsn string) (*DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(maxOpenDbConn)
+	db.SetMaxIdleConns(maxIdleDbConn)
+	db.SetConnMaxLifetime(maxDbLifeTime)
+
+	err = testDB(db)
+
+	if err != nil {
+		return nil, err
+	}
+
+	dbConn.DB = db
+
+	return dbConn, nil
+}
+
+func testDB(d *sql.DB) error {
+	err := d.Ping()
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		return err
+	}
+	fmt.Println("Success: Database is connected")
+	return nil
+}
